23d1e47857bbe6e6c6075132a79b562ba5a1afd9
Joshua Crane <josh@bintree.io>
2026-08-13 21:02:52 -0700

    added deletebeforereplace flag in images as updates were failing download for files with the same name

---
+++ proxmox/VMs/networkServices.ts
@@ -0,0 +1,1 @@
+import * as proxmox from "@pulumi/proxmox";
--- proxmox/storage/images/flatcar.ts
+++ proxmox/storage/images/flatcar.ts
@@ -6,16 +6,17 @@ import { localStorage } from "../storageConfig";
 
 
 export function flatCarIso(serverName: string) {
-    new proxmox.VirtualEnvironmentDownloadFile(`flatcar-iso-${serverName}`, {
+    new proxmox.DownloadFile(`flatcar-iso-${serverName}`, {
         contentType: 'import',
         datastoreId: 'local',
         nodeName: serverName,
         url:    "https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_proxmoxve_image.img",
-        fileName: 'flatcar_production_qemu_image.qcow2'
+        fileName: 'flatcar_production_qemu_image.qcow2',
 
     }, {
         provider: provider,
-        dependsOn: [localStorage]
+        dependsOn: [localStorage],
+        deleteBeforeReplace: true
     });
 }
 
