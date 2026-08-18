541da2a4ff7dbdc7bd42b16929930c8352059784
Joshua Crane <josh@bintree.io>
2026-08-17 19:18:02 -0700

    refactored img code into a module so that the ids can be refered by pulumi more easily

--- proxmox/storage/images/flatcar.ts
+++ proxmox/storage/images/flatcar.ts
@@ -2,18 +2,29 @@ import * as proxmox from "@pulumi/proxmox";
 import { provider } from "../../provider";
 import { localStorage } from "../storageConfig";
 
-export function flatCarIso(serverName: string) {
-    return new proxmox.DownloadFile(`flatcar-iso-${serverName}`, {
-        contentType: 'import',
-        datastoreId: 'local',
-        nodeName: serverName,
-        url:    "https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_proxmoxve_image.img",
-        fileName: 'flatcar_production_qemu_image.qcow2',
 
-    }, {
-        provider: provider,
-        dependsOn: [localStorage],
-        deleteBeforeReplace: true
-    });
+const cache = new Map<string, proxmox.DownloadFile>();
+
+export function flatCarIso(serverName: string): proxmox.DownloadFile {
+    let img : proxmox.DownloadFile | undefined = cache.get(serverName)
+    if (!img) {
+
+        img = new proxmox.DownloadFile(`flatcar-iso-${serverName}`, {
+            contentType: 'import',
+            datastoreId: 'local',
+            nodeName: serverName,
+            url: "https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_proxmoxve_image.img",
+            fileName: 'flatcar_production_qemu_image.qcow2',
+
+        }, {
+            provider: provider,
+            dependsOn: [localStorage],
+            deleteBeforeReplace: true,
+            protect: true
+        });
+        cache.set(serverName, img);
+    }
+
+    return img
 }
 
