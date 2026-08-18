8bf4890c24b10b377209bf50ccf00ab8c71c5edb
Joshua Crane <josh@bintree.io>
2026-08-17 19:18:02 -0700

    refactored img code into a module so that the ids can be refered by pulumi more easily

--- proxmox/storage/images/talos.ts
+++ proxmox/storage/images/talos.ts
@@ -2,17 +2,27 @@ import * as proxmox from "@pulumi/proxmox";
 import { provider } from "../../provider";
 import { localStorage } from "../storageConfig";
 
-export function talosIso(serverName: string) {
-    new proxmox.DownloadFile(`talos-iso-${serverName}`, {
-        contentType: 'iso',
-        datastoreId: 'local',
-        nodeName: serverName,
-        url:    "https://pxe.factory.talos.dev/image/376567988ad370138ad8b2698212367b8edcb69b5fd68c80be1f2ec7d603b4ba/v1.13.8/metal-amd64.iso",
-        fileName: 'talos-v1.10.x-metal-amd64.iso',
-    }, {
+const cache = new Map<string, proxmox.DownloadFile>();
 
-        provider: provider,
-        dependsOn: [localStorage],
-        deleteBeforeReplace: true
-    });
+export function talosIso(serverName: string): proxmox.DownloadFile {
+    let img : proxmox.DownloadFile | undefined = cache.get(serverName)
+    if (!img) {
+
+        img = new proxmox.DownloadFile(`talos-iso-${serverName}`, {
+            contentType: 'import',
+            datastoreId: 'local',
+            nodeName: serverName,
+            url:    "https://pxe.factory.talos.dev/image/376567988ad370138ad8b2698212367b8edcb69b5fd68c80be1f2ec7d603b4ba/v1.13.8/metal-amd64.iso",
+            fileName: 'talos-v1.10.x-metal-amd64.iso',
+
+        }, {
+            provider: provider,
+            dependsOn: [localStorage],
+            deleteBeforeReplace: true,
+            protect: true
+        });
+        cache.set(serverName, img);
+    }
+
+    return img
 }
