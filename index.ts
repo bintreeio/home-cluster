c816fab6392c2092f28e3074bf5ffbf50c05a4e5
Joshua Crane <josh@bintree.io>
2026-08-17 23:58:45 -0700

    any other files might of missed with refactoring

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
-import {deployImages} from "./proxmox/storage/deployImages";
-import { testVm } from "./proxmox/VMs/networkServices";
+import { deployFlatCarVM } from "./proxmox/VMs/deployFlatCar";
 
-//
-//testVm
-//deployImages()
+const networkVm01 = deployFlatCarVM("networkvm01", "pve01", {
+    ipAddress: "172.16.32.11/24",
+    gateway: "172.16.32.1",
+    vlanId: 10,
+});
+
+const networkVm02 = deployFlatCarVM("networkvm02", "pve02", {
+    ipAddress: "172.16.32.12/24",
+    gateway: "172.16.32.1",
+    vlanId: 10,
+});
---
+++ proxmox/VMs/deployFlatCar.ts
@@ -0,0 +1,62 @@
+import * as pulumi from "@pulumi/pulumi";
+import {ImageName, osImage} from "../storage/images/imageCatalog";
+import * as proxmox from "@pulumi/proxmox";
+import {ignitionSnippetBase} from "../storage/cloud-init/flatcarBase";
+import {provider} from "../provider";
+import {pveNode} from "../../utils/checkpvehosts";
+
+interface VmArgs {
+    image?: ImageName;      // defaults to flatcar
+    cores?: number;
+    memoryMb?: number;
+    osDiskSize?: number;
+    dataDiskSize?: number;
+    vlanId?: number;
+    vmName?: string;
+    ipAddress?: string;
+    gateway?: string
+    userDataFileId?: pulumi.Input<string>;
+    protect?: boolean;
+    retainOnDelete?: boolean;
+}
+
+export function deployFlatCarVM(hostName: string, pxeHostName: string, args: VmArgs = {}) : proxmox.VirtualEnvironmentVm {
+    const node = pveNode(pxeHostName);
+    return new proxmox.VirtualEnvironmentVm(hostName, {
+            name: args.vmName ?? hostName,
+            stopOnDestroy: true,
+            nodeName: node.name, //pve01, pve02, pve03
+            cpu: {
+                cores: args.cores ?? 2,
+                type: "host",
+            },
+            memory: { dedicated: args.memoryMb ?? 2048},
+            disks: [
+                { interface: "virtio0", datastoreId: "local-lvm", size: args.osDiskSize ?? 16, importFrom: osImage('flatcar', pxeHostName).id},
+                { interface: "virtio1", datastoreId: "local-lvm", size: args.dataDiskSize ?? 20, },
+            ],
+            initialization: {
+                datastoreId: "local-lvm",
+                userDataFileId: args.userDataFileId ?? ignitionSnippetBase(node.name).id,
+                ipConfigs: [
+                    {
+                        ipv4: args.ipAddress
+                            ? { address: args.ipAddress, gateway: args.gateway }
+                            : { address: "dhcp" },
+                    },
+                ],
+            },
+
+            networkDevices: [{
+                bridge: "vmbr0",
+                vlanId: args.vlanId ?? 10,
+            }],
+
+
+        }, {
+            protect: args.protect ?? false,
+            retainOnDelete: args.retainOnDelete ?? false,
+            provider: provider,}
+    );
+
+}
