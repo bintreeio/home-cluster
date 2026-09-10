8a1eae983c18e703b2868394f90a177d0bd91b78
Joshua Crane <josh@bintree.io>
2026-09-10 01:29:12 -0700

    Modified DNS configuration to ensure that dns hosts will resolve to external networks as cant pull images without. Updated pulumi configration to allow initial DNS config. updated README for docker and updated caddyfile to resolve properly.

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -5,6 +5,7 @@ import { deployDebianVM } from "./proxmox/VMs/deployDebianOS";
 const network01 = deployDebianVM("network01", "pve01", {
     ipAddress: "172.16.32.11/24",
     gateway: "172.16.32.1",
+    dnsServers: ["1.1.1.1", "9.9.9.9"],
     tags: ["dns"]
 });
 
@@ -12,6 +13,7 @@ const network01 = deployDebianVM("network01", "pve01", {
 const network02 = deployDebianVM("network02", "pve02", {
      ipAddress: "172.16.32.12/24",
      gateway: "172.16.32.1",
+     dnsServers: ["1.1.1.1", "9.9.9.9"],
      tags: ["dns"],
      protect: false
 
