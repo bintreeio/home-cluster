d222671e5cd7bc7fdb46f8cc94c21271e96d9467
Joshua Crane <josh@bintree.io>
2026-09-17 11:23:49 -0700

    changes

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -6,6 +6,7 @@ const network01 = deployDebianVM("network01", "pve01", {
     ipAddress: "172.16.32.11/24",
     gateway: "172.16.32.1",
     dnsServers: ["1.1.1.1", "9.9.9.9"],
+    memoryMb: 4096,
     tags: ["dns"]
 });
 
@@ -14,6 +15,7 @@ const network02 = deployDebianVM("network02", "pve02", {
      ipAddress: "172.16.32.12/24",
      gateway: "172.16.32.1",
      dnsServers: ["1.1.1.1", "9.9.9.9"],
+     memoryMb: 4096,
      tags: ["dns"],
      protect: false
 
